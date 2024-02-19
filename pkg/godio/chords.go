1c9b7645f8df6290f7c4b5b28818c5e5305548fe
Kim Desrosiers <kimdesro@gmail.com>
2024-02-18 21:24:39 -0500

    Improve chord notation support.

--- pkg/godio/chords.go
+++ pkg/godio/chords.go
@@ -5,10 +5,12 @@ import (
 )
 
 type Chord struct {
-	OctaveMod  string
-	Root       string
-	Type       string
-	Extensions []string
+	OctaveMod   string
+	Root        string
+	Type        string
+	Extension   string
+	Alterations []string
+	Addition    string
 }
 
 var ChordFormulas = map[string][]int{
@@ -20,16 +22,27 @@ var ChordFormulas = map[string][]int{
 	"sus4": {0, 5, 7},
 }
 
-var ExtensionInterval = map[string]int{
-	"7":   10,
-	"b9":  13,
-	"9":   14,
-	"#9":  15,
-	"b11": 16,
-	"11":  17,
-	"#11": 18,
-	"b13": 20,
-	"13":  21,
+var ExtensionIntervals = map[string][]int{
+	"6":  {9},
+	"7":  {10},
+	"9":  {10, 14},
+	"11": {10, 14, 17},
+	"13": {10, 14, 17, 21},
+}
+
+var AlterationMap = map[string]int{
+	"5":  7,
+	"6":  9,
+	"7":  10,
+	"9":  14,
+	"11": 17,
+	"13": 21,
+}
+
+var AdditionalNotes = map[string]int{
+	"add9":  14,
+	"add11": 17,
+	"add13": 21,
 }
 
 func (c Chord) GetFrequencies() []float64 {
@@ -40,9 +53,28 @@ func (c Chord) GetFrequencies() []float64 {
 		rootNoteName = c.Root + "4"
 	}
 	frequencies = append(frequencies, NoteFrequencies[rootNoteName])
-	for _, ext := range c.Extensions {
-		formula = append(formula, ExtensionInterval[ext])
+	if c.Extension != "" {
+		formula = append(formula, ExtensionIntervals[c.Extension]...)
 	}
+	if c.Addition != "" {
+		formula = append(formula, AdditionalNotes[c.Addition])
+	}
+	for _, alteration := range c.Alterations {
+		mod, degree := alteration[0], alteration[1:]
+		var newValue int
+		if mod == '#' {
+			newValue = AlterationMap[degree] + 1
+		} else if mod == 'b' {
+			newValue = AlterationMap[degree] - 1
+		}
+
+		for i, interval := range formula {
+			if interval == AlterationMap[degree] {
+				formula[i] = newValue
+			}
+		}
+	}
+
 	for _, interval := range formula {
 		frequencies = append(frequencies, NoteFrequencies.GetNoteFromInterval(rootNoteName, interval))
 	}
@@ -50,7 +82,7 @@ func (c Chord) GetFrequencies() []float64 {
 }
 
 func ParseChord(chordStr string) Chord {
-	regex := regexp.MustCompile(`(l?)([A-G][#b]?)((?:m|maj|dim|aug|sus2|sus4)?)(\d*)`)
+	regex := regexp.MustCompile(`(l?)([A-G][#b]?)((?:m|maj|dim|aug|sus2|sus4)?)((?:6|7|9|11|13)?)((?:[#b]\d{1,2})*)((?:add\d{1,2})?)`)
 
 	matches := regex.FindStringSubmatch(chordStr)
 
@@ -61,18 +93,18 @@ func ParseChord(chordStr string) Chord {
 	if chordType == "" {
 		chordType = "maj"
 	}
-	extensions := matches[4]
+	extension := matches[4]
+	alterationMatch := matches[5]
+	addition := matches[6]
 
-	// Convert extensions to slice of strings
-	var exts []string
-	for _, char := range extensions {
-		exts = append(exts, string(char))
-	}
+	alterations := regexp.MustCompile(`[#b]\d{1,2}`).FindAllString(alterationMatch, -1)
 
 	return Chord{
-		OctaveMod:  octaveModifier,
-		Root:       root,
-		Type:       chordType,
-		Extensions: exts,
+		OctaveMod:   octaveModifier,
+		Root:        root,
+		Type:        chordType,
+		Extension:   extension,
+		Alterations: alterations,
+		Addition:    addition,
 	}
 }
