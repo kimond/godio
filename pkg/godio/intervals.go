650cd1482c098aec12bd80bc936cf626f75c87b9
Kim Desrosiers <kimdesro@gmail.com>
2024-04-28 13:52:37 -0400

    Add interval semitones mapping.

---
+++ pkg/godio/intervals.go
@@ -0,0 +1,17 @@
+package godio
+
+var IntervalSemitones = map[string]int{
+	"P1": 0,
+	"m2": 1,
+	"M2": 2,
+	"m3": 3,
+	"M3": 4,
+	"P4": 5,
+	"TT": 6,
+	"P5": 7,
+	"m6": 8,
+	"M6": 9,
+	"m7": 10,
+	"M7": 11,
+	"P8": 12,
+}
